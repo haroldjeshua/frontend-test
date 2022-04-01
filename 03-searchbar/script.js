e5151ffa59434b304bf2d63f7f23d16b4e72d3de
Harold Jeshua Varde <hjvarde@integritynet.biz>
2022-04-01 19:07:08 +0800

    remove test var

--- 03-searchbar/script.js
+++ 03-searchbar/script.js
@@ -1,5 +1,3 @@
-var test = document.querySelectorAll('.card');
-
 const userCardTemplate = document.querySelector('[data-user-template]')
 const userCardContainer  = document.querySelector('[data-user-cards-container]')
 const searchInput  = document.querySelector('[data-search]')
