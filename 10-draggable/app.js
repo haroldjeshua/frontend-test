d538e64f9abc8b15d4710cffde42a365c8b66785
Harold Jeshua Varde <harvardii15@gmail.com>
2022-04-16 00:41:02 +0800

    test draggable transition and width

--- 10-draggable/app.js
+++ 10-draggable/app.js
@@ -2,9 +2,22 @@ const sortable = new Draggable.Sortable(document.querySelector(".drag-list"), {
   draggable: ".drag-list__item",
 });
 
+sortable.on('sortable:start', () => {
+	console.log('sortable:start')
+})
+sortable.on('sortable:sort', () => {
+	console.log('sortable:sort')
+})
+sortable.on('sortable:sorted', () => {
+	console.log('sortable:sorted')
+})
+sortable.on('sortable:stop', () => {
+	console.log('sortable:stop')
+})
+
 const colors = ["red", "blue", "yellow", "green", "orange", "pink"];
 const listItems = document.querySelectorAll(".drag-list__item");
 for (let i = 0; i < listItems.length; i++) {
 	const randomColor = colors[Math.floor(Math.random() * colors.length)]
-	listItems[i].style.backgroundColor = colors[i];
+	listItems[i].style.backgroundColor = randomColor
 }
